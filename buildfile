7a49e7a6ea42b76d21dba32a36b780d147ade7a0
phoet <phoetmail@googlemail.com>
2010-04-13 20:59:56 +0200

    simplify java example and update doc

--- buildfile
+++ buildfile
@@ -1,14 +1,5 @@
 repositories.remote << "http://www.ibiblio.org/maven2/"
 
-desc "The Ruby-java-jruby-rjb project"
 define "ruby-java-jruby-rjb", :version=>"0.0.1" do
-  compile.with "org.jruby:jruby:jar:1.0.1", 
-  "bsf:bsf:jar:2.4.0", 
-  "commons-logging:commons-logging:jar:1.1.1",
-  "jline:jline:jar:0.9.94",
-  "backport-util-concurrent:backport-util-concurrent:jar:3.0",
-  "asm:asm:jar:2.2.3",
-  "asm:asm-commons:jar:2.2.3"
-  
-  package(:jar)
+  compile.with "org.jruby:jruby-complete:jar:1.4.0"
 end
--- src/test/java/de/nofail/jruby/JRubyTest.java
+++ src/test/java/de/nofail/jruby/JRubyTest.java
@@ -1,27 +1,17 @@
 package de.nofail.jruby;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.apache.bsf.BSFException;
-import org.apache.bsf.BSFManager;
+import org.jruby.embed.ScriptingContainer;
 import org.junit.Test;
 
+/**
+ * Simple example from http://kenai.com/projects/jruby/pages/RedBridge
+ * 
+ * @author nofail
+ */
 public class JRubyTest {
 
 	@Test
-	public void execute_jruby_with_bsf() throws MalformedURLException, BSFException {
-		// http://www.javaworld.com/javaworld/jw-07-2006/jw-0717-ruby.html
-		// JRuby must be registered in BSF.
-		// jruby.jar and bsf.jar must be on classpath.
-		BSFManager.registerScriptingEngine("ruby", "org.jruby.javasupport.bsf.JRubyEngine", new String[] { "rb" });
-		BSFManager manager = new BSFManager();
-		// Make the variable myUrl available from Ruby.
-		manager.declareBean("myUrl", new URL("http://www/jruby.org"), URL.class);
-		// Note that the Method getDefaultPort is available from Ruby
-		// as getDefaultPort and also as defaultPort.
-		// The following line illustrates the combination of Ruby syntax
-		// and a Java method call.
-		manager.eval("ruby", "(java)", 1, 1, "puts 'hello world'");
+	public void execute_jruby_scriptlet() {
+		new ScriptingContainer().runScriptlet("puts 'hello world'");
 	}
 }
